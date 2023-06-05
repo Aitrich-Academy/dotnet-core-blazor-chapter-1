83137b84fd4740c9a66f84987d0e74ebb8cd20ed
Anuvind <soudha.aitrich@gmail.com>
2023-06-05 11:52:11 +0530

    Commit

--- HireMeNow-BlazorApp/HireMeNow-BlazorApp/Interfaces/IApplicationService.cs
+++ HireMeNow-BlazorApp/HireMeNow-BlazorApp/Interfaces/IApplicationService.cs
@@ -1,7 +1,10 @@
-﻿namespace HireMeNow_BlazorApp.Interfaces
+﻿
+using HireMeNow_BlazorApp.Models;
+namespace HireMeNow_BlazorApp.Interfaces
 {
 	public interface IApplicationService
 	{
-		void AddApplication(Guid JobId, Guid UserId);
+        public List<Application> GetAll(Guid userId);
+        void AddApplication(Guid JobId, Guid UserId);
 	}
 }
